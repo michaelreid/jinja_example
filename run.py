53d9f810db96287255e4678ad4c13784acee5730
michael reid <michaelreid@users.noreply.github.com>
2015-03-15 01:44:36 +0000

    Test the macro function in Jinja syntax.

--- run.py
+++ run.py
@@ -17,6 +17,26 @@ def template_test():
     return render_template('template.html', my_string="Wheeeee!",
                            my_list=[0,1,2,3,4,5])
 
+# setup navigation
+@app.route("/home")
+def home():
+    return render_template('template.html',
+                           my_string="I'm the home page",
+                           my_list=[0,1,2,3,4,5])
+
+@app.route("/about")
+def about():
+    return render_template('template.html',
+                           my_string="I'm the about page",
+                           my_list=[0,1,2,3,4,5])
+    
+@app.route("/contact")
+def contact():
+    return render_template('template.html',
+                           my_string="I'm the contact page",
+                           my_list=[0,1,2,3,4,5])
+
+
 # define __main__ function so can call from command line
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
